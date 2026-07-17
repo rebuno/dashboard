a228dbef3d987baa364d739ef81b71c21b8ac548
nathanvvong <nathanw1510@gmail.com>
2026-07-17 13:08:38 -0700

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
-  title: "Rebuno Dashboard",
+  title: "Rebuno",
   description: "Rebuno dashboard",
 };
 
