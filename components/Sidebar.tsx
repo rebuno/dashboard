b545281b22e164105c00ea2a64088fc117674dc6
Kasra Soltani <54288687+Kasoltani@users.noreply.github.com>
2026-08-29 16:31:07 -0700

    replace navbar text with logo
    - Replaced the "rebuno" text with company logo
    - Added the SVG asset to the dashboard

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
@@ -35,7 +36,13 @@ export default function Sidebar() {
   return (
     <aside className="w-56 shrink-0 border-r border-gray-200 bg-white flex flex-col">
       <div className="h-14 flex items-center px-4 border-b border-gray-200">
-        <span className="font-semibold text-lg">rebuno</span>
+        <Image
+          src="/rebuno-light.svg"
+          alt="Rebuno"
+          width={1200}
+          height={548}
+          className="h-8 w-auto"
+        />
       </div>
       <nav className="flex-1 py-2">
         {NAV_ITEMS.map((item) => {
